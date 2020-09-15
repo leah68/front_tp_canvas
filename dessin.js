84a905e2d149741693acff42dcae023373dcaa95
leah68 <leahartmann68@gmail.com>
2020-09-15 09:55:16 +0200

    ajout trait bleu

--- dessin.js
+++ dessin.js
@@ -4,3 +4,15 @@ canvas.style.border = "1px solid black";
 
 ctx.fillStyle = 'red';
 ctx.fillRect(10, 10, 200, 100);
+
+ctx.beginPath();
+ctx.moveTo(100, 150);
+ctx.lineTo(200, 250);
+ctx.lineWidth = 10;
+ctx.lineCap = 'round';
+ctx.strokeStyle = 'blue';
+ctx.fillStyle = 'cyan';
+ctx.fill();
+ctx.stroke();
+
+
