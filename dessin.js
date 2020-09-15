aa4f85f04f82f22ac77fa6fe2a3b93f4d426acfc
leah68 <leahartmann68@gmail.com>
2020-09-15 09:48:25 +0200

    test carré rouge

--- dessin.js
+++ dessin.js
@@ -1,2 +1,6 @@
 const canvas = document.getElementById('monCanvas');
 const ctx = canvas.getContext('2d');
+canvas.style.border = "1px solid black";
+
+ctx.fillStyle = 'red';
+ctx.fillRect(10, 10, 200, 100);
